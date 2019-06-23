36f8b86bd5b22434a0dda56f7e65833cfa4a7b35
mike stedman <barbeque@users.noreply.github.com>
2019-06-23 11:34:28 -0600

    Dumping into an autogenerated struct

--- d88.py
+++ d88.py
@@ -1,4 +1,5 @@
-from struct import unpack, calcsize
+from struct import unpack, calcsize, Struct
+import sys
 
 # D88 header:
 # 	char title[17];
@@ -7,7 +8,9 @@ from struct import unpack, calcsize
 #	BYTE type;
 #	DWORD size;
 #	DWORD trkptr[164];
-d88_header_fmt = '17s9s1s1s<i<164i'
+d88_header_fmt = '<17s9s1s1s1i164i'
+d88_header_len = calcsize(d88_header_fmt)
+d88_header_unpack = Struct(d88_header_fmt).unpack_from
 
 # D88 track header:
 #	BYTE c, h, r, sector_size;
@@ -15,3 +18,15 @@ d88_header_fmt = '17s9s1s1s<i<164i'
 #	BYTE density, del, stat;
 #	BYTE rsrv[5];
 #	WORD size;
+
+def usage():
+    print sys.argv[0] + " [d88 file]"
+
+if len(sys.argv) < 2:
+    usage()
+    sys.exit(1)
+
+with open(sys.argv[1], 'rb') as f:
+    raw = f.read(d88_header_len)
+    header = d88_header_unpack(raw)
+    print header
