66d7df35b9aa9cb2a44ec5b91b1a16b6c5b850b9
mike stedman <barbeque@users.noreply.github.com>
2019-06-23 11:09:10 -0600

    D88

---
+++ d88.py
@@ -0,0 +1,14 @@
+# D88 header:
+# 	char title[17];
+#   BYTE rsrv[9];
+#	BYTE protect;
+#	BYTE type;
+#	DWORD size;
+#	DWORD trkptr[164];
+
+# D88 track header:
+#	BYTE c, h, r, sector_size;
+#	WORD nsec;
+#	BYTE density, del, stat;
+#	BYTE rsrv[5];
+#	WORD size;
