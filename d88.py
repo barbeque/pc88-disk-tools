7cff5edf02077ba0bf9477e616987540552fe50d
mike stedman <barbeque@users.noreply.github.com>
2019-06-24 17:05:05 -0600

    Enum decodes

--- d88.py
+++ d88.py
@@ -1,5 +1,6 @@
 from struct import unpack, calcsize, Struct
 import sys
+import array
 
 # D88 header:
 # 	char title[17];
@@ -7,8 +8,8 @@ import sys
 #	BYTE protect;
 #	BYTE type;
 #	DWORD size;
-#	DWORD trkptr[164];
-d88_header_fmt = '<17s9s1s1s1i164i'
+#	DWORD trkptr[164] - can't decode directly, put in another structure;
+d88_header_fmt = '<17s9sBB1i'
 d88_header_len = calcsize(d88_header_fmt)
 d88_header_unpack = Struct(d88_header_fmt).unpack_from
 
@@ -18,9 +19,9 @@ d88_header_unpack = Struct(d88_header_fmt).unpack_from
 #	BYTE density, del, stat;
 #	BYTE rsrv[5];
 #	WORD size;
-track_header_fmt = '<ccccsccc5cs'
-track_header_len = calcsize(track_header_fmt)
-track_header_unpack = Struct(track_header_fmt).unpack_from
+sector_header_fmt = '<BBBBsBBB5sI'
+sector_header_len = calcsize(sector_header_fmt)
+sector_header_unpack = Struct(sector_header_fmt).unpack_from
 
 def usage():
     print sys.argv[0] + " [d88 file]"
@@ -29,7 +30,51 @@ if len(sys.argv) < 2:
     usage()
     sys.exit(1)
 
+def sector_size_to_bytes(sector_size):
+    if sector_size == 0:
+        return 128
+    elif sector_size == 1:
+        return 256
+    elif sector_size == 2:
+        return 512
+    elif sector_size == 3:
+        return 1024
+    else:
+        raise "Unknown sector size " + sector_size
+
+def density_to_string(density):
+    if density == 0:
+        return "double"
+    elif density == 1:
+        return "high"
+    else:
+        raise "Unknown density " + density + ", could be single?"
+
 with open(sys.argv[1], 'rb') as f:
     raw = f.read(d88_header_len)
-    header = d88_header_unpack(raw)
-    print header
+    d88_header = d88_header_unpack(raw)
+    (title, rsrv, protect, type, size) = d88_header
+    tracks = array.array('I')
+    tracks.read(f, 164) # trkptr structure
+    tracks = tracks.tolist()
+    print 'Filename:', sys.argv[1]
+    print 'Title:', title
+    #print 'Tracks:', tracks
+    actual_tracks = filter(lambda x: x > 0, tracks)
+    print 'Tracks actually in use:', len(actual_tracks)
+
+    # i suspect it goes TRK - SEC and track headers are the same
+    i = 0
+    for track_origin in actual_tracks:
+        print 'Track #', i
+
+        f.seek(track_origin)
+        raw = f.read(sector_header_len)
+        track_header = sector_header_unpack(raw)
+        #print track_header
+        (c, h, r, sector_size, nsec, density, _del, stat, rsrv, size) = track_header
+        print 'Sector size (in bytes):', sector_size_to_bytes(sector_size)
+        print 'Density:', density_to_string(density)
+        break # FIXME
+
+        i += 1
